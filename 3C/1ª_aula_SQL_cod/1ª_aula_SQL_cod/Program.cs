34e4e9065c1e3f136e5c8225a00a32ef0b82cc82
fsewafwet <gterwds>
2017-11-13 15:51:23 -0300

    sql

---
+++ 3C/1ª_aula_SQL_cod/1ª_aula_SQL_cod/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1ª_aula_SQL_cod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
